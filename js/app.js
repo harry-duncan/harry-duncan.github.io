fb5c9ad26aa18f9c20af3b5fd3ea965340c34a7e
Harry Duncan <harryduncan25@gmail.com>
2016-02-24 00:01:42 +1100

    added breakpoints laptop and ipad

--- js/app.js
+++ js/app.js
@@ -16,3 +16,11 @@ $(function() {
 });
 
 console.log("Something else");
+
+// nav
+$(document).ready(function(){
+  $('.bars').click(function(){
+    $('#nav').toggleClass('open');
+    $('.container').toggleClass('menu-open');
+  });
+});
